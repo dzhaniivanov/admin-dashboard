f84c060e77a6c52689dd49c78505156924f872c9
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2024-06-11 14:58:16 +0300

    added dashoard with hardcoded data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import { Button } from "@/components/ui/button";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl">Dashboard</h1>
-    <Button>Click me</Button>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          count={100}
+          icon={<Newspaper className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500" size={72} />}
+        />
+      </div>
+      <PostsTable title="Latest Posts" limit={5} />
     </>
-   
   );
 }
